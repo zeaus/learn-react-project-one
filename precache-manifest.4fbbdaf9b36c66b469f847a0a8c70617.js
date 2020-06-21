ca0e7588c0ab7b505c1d11253357261e810910f5
Taha Shahid <tahashahid101010@gmail.com>
2020-06-21 20:36:20 +0500

    Updates

---
+++ precache-manifest.4fbbdaf9b36c66b469f847a0a8c70617.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1d53afff1ba86e817961fe87fe50242",
+    "url": "/learn-react-project-one/index.html"
+  },
+  {
+    "revision": "e97acd4cb0d7773704cf",
+    "url": "/learn-react-project-one/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "531b352578e86d1ab256",
+    "url": "/learn-react-project-one/static/js/2.d3501cd6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/learn-react-project-one/static/js/2.d3501cd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e97acd4cb0d7773704cf",
+    "url": "/learn-react-project-one/static/js/main.e9ff246f.chunk.js"
+  },
+  {
+    "revision": "58877252b205997476ae",
+    "url": "/learn-react-project-one/static/js/runtime-main.eeaf1985.js"
+  }
+]);
